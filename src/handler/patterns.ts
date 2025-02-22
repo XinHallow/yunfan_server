ec69378e20a3768d521d9fc158125f05c81008f2
xin hallow <xinhallow@outlook.com>
2025-02-22 16:10:31 +0800

    feat(1 file): 修改基准url

--- src/handler/patterns.ts
+++ src/handler/patterns.ts
@@ -1,4 +1,4 @@
-const base_url = "http://localhost:8000/";
+const base_url = "https://yunfan-resource.deno.dev/";
 
 export const homework_default = new URLPattern("/homework", base_url);
 export const homework_date = new URLPattern("/homework?date=:date", base_url);
